aef4cdb57dbc0e4000150ff218928e5619cbb58d
mJeylanii <95252871+mJeylanii@users.noreply.github.com>
2022-03-17 15:30:57 +0300

    DOM

--- scripts/caesars-encryption.js
+++ scripts/caesars-encryption.js
@@ -1,12 +1,26 @@
+
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var newalpha = "";
+let n = 3;
+
+var message="";
+var encrypted = document.getElementById('caesar_encrypted');
+
+function display()
+{
+    shift(3);
+    message = document.getElementById("caesar_original").value;
+   encrypted.innerHTML = encode(message);
+    
+}
+
 
 function shift(n){
     for (let i = 0; i < alphabet.length; i++){
         let offset = (i + n) % alphabet.length;
         newalpha += alphabet[offset];
     }
-}
+} 
 
 function encode(message){
     let result = "";
@@ -17,6 +31,7 @@ function encode(message){
     }
     return result;
 }
+
 
 function decode(message){
     let result = "";
