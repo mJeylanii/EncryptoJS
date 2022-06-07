5cd970db6f215801aa836e83669a678b19eeb76b
Mohamed Jeylani <95252871+mJeylanii@users.noreply.github.com>
2022-06-07 09:47:48 +0300

    Update caesars-encryption.js

--- scripts/caesars-encryption.js
+++ scripts/caesars-encryption.js
@@ -2,18 +2,19 @@
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var newalpha = "";
 let text="";
-const n = 2;
+const n = document.getElementById('shifts').value ;
 //Function to be called when button clicked
 function displayencode()
 {
-   shift(n);
+    
+    shift(n);
    text = document.getElementById("caesar_original").value;
    document.getElementById('caesar_encrypted').value = encode(text);
    document.getElementById('caesar_original').value ="";
 }
 function displaydecode()
 {
-    shift(n)
+    shift(n);
     text = document.getElementById("caesar_encrypted").value;
     document.getElementById('caesar_original').value = decode(text);
     document.getElementById('caesar_encrypted').value = "";
@@ -25,6 +26,7 @@ function shift(n){
     for (let i = 0; i < alphabet.length; i++){
         let offset = (i + n) % alphabet.length;
         newalpha += alphabet[offset];
+        console.log(newalpha);
     }
 } 
 //* Encoding function */
@@ -34,6 +36,7 @@ function encode(message){
     for (let i = 0; i < message.length; i++){
         let index = alphabet.indexOf(message[i]);
         result += newalpha[index];
+      
     }
     return result;
 }
